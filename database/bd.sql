b004a6954ffa80c60a7acaef5c17a7aa113c3931
Tobiewe <tobiewe02@gmail.com>
2023-09-19 10:45:41 +0200

    database

---
+++ database/bd.sql
@@ -0,0 +1,29 @@
+
+-- Tabla de Destinos Turísticos
+CREATE TABLE destinos (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nombre VARCHAR(255) NOT NULL,
+ descripcion TEXT,
+ imagen VARCHAR(255),
+ precio DECIMAL(10, 2)
+);
+
+-- Tabla de Reservas de Viajes
+CREATE TABLE reservas (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ destino_id INT,
+ nombre_cliente VARCHAR(255) NOT NULL,
+ correo_cliente VARCHAR(255) NOT NULL,
+ fecha_reserva DATE NOT NULL,
+ FOREIGN KEY (destino_id) REFERENCES destinos(id)
+);
+
+-- Tabla de Comentarios en Destinos
+CREATE TABLE comentarios (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ destino_id INT,
+ nombre_usuario VARCHAR(255) NOT NULL,
+ comentario TEXT NOT NULL,
+ fecha_comentario TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ FOREIGN KEY (destino_id) REFERENCES destinos(id)
+);
