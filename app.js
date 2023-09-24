494483182eeb2933950fcb80d33ef4c4f1647e1d
Tobiewe <tobiewe02@gmail.com>
2023-09-24 12:08:06 +0200

    barra de busqueda a medias

--- app.js
+++ app.js
@@ -27,3 +27,30 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor Express escuchando en el puerto ${port}`);
 });
+
+busqueda = async(req, res) => {
+  const buscar = req.query.nombreBuscar; // Obtén el término de búsqueda de la consulta GET
+
+  console.log("llegó la búsqueda");
+  console.log(buscar);
+
+  if (!buscar) {
+    // Si no se proporciona un término de búsqueda, muestra una página sin resultados
+    res.render('index', { results: [] });
+  } else {
+    // Realiza la consulta a la base de datos con el término de búsqueda
+    const query = 'SELECT * FROM destinos WHERE nombre LIKE ? OR descripcion LIKE ?';
+    const searchTerm = `%${buscar}%`;
+
+    dbConnection.query(query, [searchTerm, searchTerm], (err, results) => {
+      if (err) {
+        console.error('Error al ejecutar la consulta:', err);
+        res.status(500).json({ error: 'Error de la base de datos' });
+        return;
+      }
+
+      // Renderiza la vista "index.ejs" con los resultados de la búsqueda
+      res.render('index', { results: results });
+    });
+  }
+}
