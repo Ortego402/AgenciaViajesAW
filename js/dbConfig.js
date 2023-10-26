059f1b7f38cd1608ff67676b497967ee1b729099
VOrtego <v.ortego.martin@gmail.com>
2023-10-26 09:05:20 +0200

    Solucionado createPool

--- js/dbConfig.js
+++ js/dbConfig.js
@@ -1,22 +1,14 @@
 // Importa el módulo mysql
 const mysql = require('mysql');
 
-// Configuración de la conexión a la base de datos
-const dbConnection = mysql.createConnection({
+// Configuración de la conexión a la base de datos utilizando un pool
+const dbConnection = mysql.createPool({
+  connectionLimit: 10,     // Límite de conexiones en el pool
   host: 'localhost',       // Dirección del servidor de la base de datos
   user: 'root',            // Nombre de usuario de la base de datos
   password: '',            // Contraseña de la base de datos
-  database: 'viajes' // Nombre de la base de datos a la que conectarse
+  database: 'viajes'       // Nombre de la base de datos a la que conectarse
 });
 
-// Conecta a la base de datos
-dbConnection.connect((err) => {
-  if (err) {
-    console.error('Error de conexión a la base de datos:', err); // Muestra un mensaje de error si la conexión falla
-    return;
-  }
-  console.log('Conexión exitosa a la base de datos'); // Muestra un mensaje de éxito si la conexión es exitosa
-});
-
-// Exporta el objeto de conexión y otras funciones relacionadas con la base de datos
+// Exporta el objeto del pool de conexiones y otras funciones relacionadas con la base de datos
 module.exports = dbConnection;
