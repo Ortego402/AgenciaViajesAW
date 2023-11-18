1bd4a61e6ac4b702d3704f78ae0f8a06eab672a7
VOrtego <v.ortego.martin@gmail.com>
2023-11-18 18:35:40 +0100

    Borrado lo viejo

---
+++ myapp/app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+
+var destinoRouter = require('./routes/destinos');
+var usersRouter = require('./routes/users');
+
+const app = express();
+const port = 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // Parse application/x-www-form-urlencoded
+app.use(cookieParser());
+
+// Configuración de la sesión
+app.use(session({
+  secret: 'Epicscape', // Secreto para firmar la cookie de sesión
+  resave: false,
+  saveUninitialized: true
+}));
+
+// Configuración del motor de vistas y carpeta de vistas
+app.set('view engine', 'ejs'); // Motor de vistas EJS
+app.set("views", path.join(__dirname, 'views')); // Carpeta de vistas
+
+// Middleware para archivos estáticos (CSS, imágenes, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Usa los enrutadores
+app.use('/', destinoRouter);
+app.use('/users', usersRouter);
+
+// Captura el error 404 y lo pasa al manejador de errores
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Manejador de errores
+app.use(function(err, req, res, next) {
+  // Establece las variables locales, solo proporcionando el error en desarrollo
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Renderiza la página de error
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Inicia el servidor en el puerto especificado
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
+
+module.exports = app;
---
+++ myapp/routes/destinos.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require("mysql");
+const config = require("../config/dbConfig");
+const DAODestino = require("../dao/DAODestino");
+
+// Crear un pool de conexiones a la base de datos de MySQL 
+const pool = mysql.createPool(config.mysqlConfig);
+
+daoDestino = new DAODestino(pool);
+
+// Página principal
+router.get('/', (req, res) => {
+  daoDestino.getAllDestinos((err, results) => {
+      if (err) {
+          return res.status(500).json({ error: 'Error de la base de datos' });
+      }
+      return res.render('home.ejs', { results: results, session: req.session });
+  });
+});
+
+// Página de destinos populares
+router.get('/populares', (req, res) => {
+  return res.render("populares.ejs", { session: req.session });
+});
+
+// Página "Nosotros"
+router.get('/nosotros', (req, res) => {
+  return res.render("nosotros.ejs", { session: req.session });
+});
+
+// Página de servicios
+router.get('/servicios', (req, res) => {
+  return res.render("servicios.ejs", { session: req.session });
+});
+
+
+// Página de búsqueda de destinos
+router.get('/buscar', (req, res) => {
+  const searchTerm = req.query.nombreBuscar;
+  daoDestino.searchDestinos(searchTerm, (err, results) => {
+      if (err) {
+          return res.status(500).json({ error: 'Error de la base de datos' });
+      }
+      return res.render('home.ejs', { results: results, session: req.session });
+  });
+});
+
+// Publicar comentario en un destino específico
+router.post('/:id/comentarios', (req, res) => {
+    destinoSA.comentarDestino(req, res, (err) => {
+        return res.redirect(`/${req.params.id}?mensaje=${encodeURIComponent(err)}`);
+    });
+});
+
+// Reservar un destino específico
+router.post('/:id/reservar', (req, res) => {
+  const { fecha_reserva } = req.body;
+  const id = req.params.id;
+  
+  daoDestino.insertarReserva(id, req.session.username, fecha_reserva, (err) => {
+      return res.redirect(`/${req.params.id}?mensaje=${encodeURIComponent(err)}`);
+  });
+});
+
+// Mostrar un destino específico
+router.get('/:id', (req, res) => {
+
+  const id = req.params.id;
+  const reservaConfirmada = req.query.reserva === 'confirmada';
+  const comentarioConfirmado = req.query.comentario === 'confirmada';
+
+  let mensaje = '';
+  if (reservaConfirmada) {
+      mensaje = '¡Reserva completada! Gracias por realizar la reserva.';
+  } else if (comentarioConfirmado) {
+      mensaje = 'Comentario realizado correctamente.';
+  } else if (req.query.reserva === 'null' || req.query.comentario === 'null') {
+      mensaje = '¡Ups! Ha ocurrido un error al realizar la acción.';
+  }
+
+  daoDestino.getDestinoById(id, (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: 'Error de la base de datos' });
+      }
+      
+      // Obtener imágenes del destino
+      daoDestino.getImagenesByDestinoId(id, (err, results) => {
+        if (err) {
+          return res.status(500).json({ error: 'Error de la base de datos' });
+        }
+
+        // Obtener comentarios del destino
+        daoDestino.getComentariosByDestinoId(id, (err, comentarios) => {
+          if (err) {
+            return res.status(500).json({ error: 'Error de la base de datos' });
+          }
+          else{
+            return res.render('destino.ejs', { result: result, results: results, comentarios: comentarios, session: req.session, mensaje: mensaje });
+          }
+        });
+      });
+    });
+  });
+
+module.exports = router;
